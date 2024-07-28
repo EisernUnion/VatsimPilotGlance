efc5b74e82f471cc02ffd688434bfa8168fad5cf
Basti <Basti@ferrevm-vnion.de>
2024-07-28 17:52:34 +0200

    Projektdateien hinzufügen.

---
+++ RunVPG/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+string path = Directory.GetCurrentDirectory();
+string appPath = Path.Combine(path, "vpg.exe");
+
+Console.Write("Please enter the ICAO Code to observe?: ");
+string input = Console.ReadLine();
+Console.WriteLine($"Your input: {input}");
+
+string icao = input.ToUpper();
+Process.Start(appPath, icao);
+
